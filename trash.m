7549e3edb4c413b7f77eab3262aa72f264bdf257
Dustin Sallings <dustin@spy.net>
2005-04-12 06:09:56 +0000

    initial trash implementation

---
+++ trash.m
@@ -0,0 +1,39 @@
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+#import <libgen.h>
+
+int main (int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	NSString *pathPrefix=@"./";
+
+	NSWorkspace *ws=[NSWorkspace sharedWorkspace];
+	int i=0;
+	for(i=1; i<argc; i++) {
+		int theTag=0;
+		char *p=argv[i];
+		NSString *theDir=[[NSString alloc] initWithCString: dirname(p)];
+		NSString *theFileB=[[NSString alloc] initWithCString: basename(p)];
+		NSString *theFile=[pathPrefix stringByAppendingString: theFileB];
+		NSArray *files=[[NSArray alloc] initWithObjects: theFile, nil];
+
+		// Perform the delete
+		BOOL result=[ws performFileOperation:NSWorkspaceRecycleOperation
+			source: theDir
+			destination:@""
+			files: files
+			tag: &theTag];
+
+		if(result == NO) {
+			perror(p);
+		}
+
+		[theDir release];
+		[theFileB release];
+		[files release];
+	}
+
+    [pool release];
+    return 0;
+}
